ceba779d8f2847293fafde6e697a0971ee42fc9e
Will Usher <willusher.life@gmail.com>
2014-12-28 18:08:11 -0800

    Will just do ppm images for simplicity

--- src/film/mod.rs
+++ src/film/mod.rs
@@ -1,5 +1,5 @@
 //! The film module provides color types and a render target that the image
-//! is written too. Functions are also provided for saving PPM and BMP images
+//! is written too. Functions are also provided for saving PPM images
 //! while I wait to migrate over to the Piston image library due to some
 //! compile issues TODO: https://github.com/PistonDevelopers/image
 
