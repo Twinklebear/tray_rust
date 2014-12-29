45b0cfe8062b4851e65758650684901f41fabe02
Will Usher <willusher.life@gmail.com>
2014-12-29 10:59:31 -0800

    Add alpha channel to color for storing color weights

--- src/film/color.rs
+++ src/film/color.rs
@@ -3,28 +3,33 @@
 use std::num::Float;
 use linalg;
 
-/// Colorf is a floating point RGB color type
-#[deriving(Show, Copy, PartialEq)]
+/// Colorf is a floating point RGBA color type
+/// `a` is typically used to store the weight of a color eg. in the
+/// render target for multisampling we need to track the weight to
+/// normalize in the end and is always initialized to 0
+#[deriving(Show, Copy, Clone, PartialEq)]
 pub struct Colorf {
     pub r: f32,
     pub g: f32,
     pub b: f32,
+    pub a: f32,
 }
 
 impl Colorf {
     /// Create an RGB color
     pub fn new(r: f32, g: f32, b: f32) -> Colorf {
-        Colorf { r: r, g: g, b: b }
+        Colorf { r: r, g: g, b: b, a: 0f32 }
     }
     /// Create an RGB color using the same value for all channels
     pub fn broadcast(r: f32) -> Colorf {
-        Colorf { r: r, g: r, b: r }
+        Colorf { r: r, g: r, b: r, a: 0f32 }
     }
     /// Clamp the color values between [0, 1]
     pub fn clamp(&self) -> Colorf {
         Colorf { r: linalg::clamp(self.r, 0.0, 1.0),
                  g: linalg::clamp(self.g, 0.0, 1.0),
-                 b: linalg::clamp(self.b, 0.0, 1.0) }
+                 b: linalg::clamp(self.b, 0.0, 1.0),
+                 a: linalg::clamp(self.a, 0.0, 1.0) }
     }
     /// Compute the luminance of the color
     pub fn luminance(&self) -> f32 {
@@ -50,56 +55,57 @@ impl Colorf {
     }
     /// Return the color with values { e^r, e^g, e^b }
     pub fn exp(&self) -> Colorf {
-        Colorf { r: Float::exp(self.r), g: Float::exp(self.g), b: Float::exp(self.b) }
+        Colorf { r: Float::exp(self.r), g: Float::exp(self.g),
+                 b: Float::exp(self.b), a: Float::exp(self.a) }
     }
 }
 
 impl Add<Colorf, Colorf> for Colorf {
     /// Add two colors together
     fn add(self, rhs: Colorf) -> Colorf {
-        Colorf { r: self.r + rhs.r, g: self.g + rhs.g, b: self.b + rhs.b }
+        Colorf { r: self.r + rhs.r, g: self.g + rhs.g, b: self.b + rhs.b, a: self.a + rhs.a }
     }
 }
 
 impl Sub<Colorf, Colorf> for Colorf {
     /// Subtract the two colors
     fn sub(self, rhs: Colorf) -> Colorf {
-        Colorf { r: self.r - rhs.r, g: self.g - rhs.g, b: self.b - rhs.b }
+        Colorf { r: self.r - rhs.r, g: self.g - rhs.g, b: self.b - rhs.b, a: self.a - rhs.a }
     }
 }
 
 impl Mul<Colorf, Colorf> for Colorf {
     /// Multiply the two colors
     fn mul(self, rhs: Colorf) -> Colorf {
-        Colorf { r: self.r * rhs.r, g: self.g * rhs.g, b: self.b * rhs.b }
+        Colorf { r: self.r * rhs.r, g: self.g * rhs.g, b: self.b * rhs.b, a: self.a * rhs.a }
     }
 }
 
 impl Mul<f32, Colorf> for Colorf {
     /// Scale the color by the float
     fn mul(self, rhs: f32) -> Colorf {
-        Colorf { r: self.r * rhs, g: self.g * rhs, b: self.b * rhs }
+        Colorf { r: self.r * rhs, g: self.g * rhs, b: self.b * rhs, a: self.a * rhs}
     }
 }
 
 impl Div<Colorf, Colorf> for Colorf {
     /// Divide the channels of one color by another
     fn div(self, rhs: Colorf) -> Colorf {
-        Colorf { r: self.r / rhs.r, g: self.g / rhs.g, b: self.b / rhs.b }
+        Colorf { r: self.r / rhs.r, g: self.g / rhs.g, b: self.b / rhs.b, a: self.a / rhs.a }
     }
 }
 
 impl Div<f32, Colorf> for Colorf {
     /// Divide the channels of the color by the float
     fn div(self, rhs: f32) -> Colorf {
-        Colorf { r: self.r / rhs, g: self.g / rhs, b: self.b / rhs }
+        Colorf { r: self.r / rhs, g: self.g / rhs, b: self.b / rhs, a: self.a / rhs }
     }
 }
 
 impl Neg<Colorf> for Colorf {
     /// Negate the color channels
     fn neg(self) -> Colorf {
-        Colorf { r: -self.r, g: -self.g, b: -self.b }
+        Colorf { r: -self.r, g: -self.g, b: -self.b, a: -self.a }
     }
 }
 
@@ -109,11 +115,13 @@ impl Index<uint, f32> for Colorf {
     /// - 0 = r
     /// - 1 = g
     /// - 2 = b
+    /// - 3 = a
     fn index(&self, i: &uint) -> &f32 {
         match *i {
             0 => &self.r,
             1 => &self.g,
             2 => &self.b,
+            3 => &self.a,
             _ => panic!("Invalid index into color"),
         }
     }
@@ -125,11 +133,13 @@ impl IndexMut<uint, f32> for Colorf {
     /// - 0 = r
     /// - 1 = g
     /// - 2 = b
+    /// - 3 = a
     fn index_mut(&mut self, i: &uint) -> &mut f32 {
         match *i {
             0 => &mut self.r,
             1 => &mut self.g,
             2 => &mut self.b,
+            3 => &mut self.a,
             _ => panic!("Invalid index into color"),
         }
     }
