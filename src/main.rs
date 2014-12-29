316e8db7cb25a5b21c4fa97cd1d532b084fa4401
Will Usher <willusher.life@gmail.com>
2014-12-29 14:55:05 -0800

    First ray traced image!

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 extern crate tray_rust;
 
+use tray_rust::linalg;
 use tray_rust::film;
+use tray_rust::geometry;
+use tray_rust::geometry::Geometry;
 
 fn main() {
-    let mut rt = film::RenderTarget::new(2, 2);
-    film::write_ppm("black_test.ppm", 2, 2, rt.get_render().as_slice());
-
-    rt.write(0.5, 0.5, &film::Colorf::new(1.0, 0.0, 0.0));
-    rt.write(1.5, 0.5, &film::Colorf::new(0.0, 1.0, 0.0));
-    rt.write(0.5, 1.5, &film::Colorf::new(0.0, 0.0, 1.0));
-    rt.write(1.5, 1.5, &film::Colorf::new(0.3, 0.3, 0.3));
-    film::write_ppm("out.ppm", 2, 2, rt.get_render().as_slice());
+    let width = 800u;
+    let height = 600u;
+    let mut rt = film::RenderTarget::new(width, height);
+    let camera = film::Camera::new(linalg::Transform::look_at(
+        &linalg::Point::new(0.0, 0.0, -10.0), &linalg::Point::new(0.0, 0.0, 0.0),
+        &linalg::Vector::new(0.0, 1.0, 0.0)), 40.0, rt.dimensions());
+    let sphere = geometry::Sphere::new(1.5);
+    {
+        let instance = geometry::Instance::new(&sphere,
+            linalg::Transform::translate(&linalg::Vector::new(0.0, 2.0, 0.0)));
+        for y in range(0, height) {
+            for x in range(0, width) {
+                let px = (x as f32 + 0.5, y as f32 + 0.5);
+                let mut ray = camera.generate_ray(px);
+                match instance.intersect(&mut ray) {
+                    Some(_) => rt.write(px.0, px.1, &film::Colorf::broadcast(1.0)),
+                    None => {},
+                }
+            }
+        }
+    }
+    film::write_ppm("out.ppm", width, height, rt.get_render().as_slice());
 }
 
