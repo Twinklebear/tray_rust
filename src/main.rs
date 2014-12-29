d0579b534f5266649ab571fd2e858b495cc4b808
Will Usher <willusher.life@gmail.com>
2014-12-29 15:30:51 -0800

    Don't actually need that new block

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,15 @@ fn main() {
         &linalg::Point::new(0.0, 0.0, -10.0), &linalg::Point::new(0.0, 0.0, 0.0),
         &linalg::Vector::new(0.0, 1.0, 0.0)), 40.0, rt.dimensions());
     let sphere = geometry::Sphere::new(1.5);
-    {
-        let instance = geometry::Instance::new(&sphere,
-            linalg::Transform::translate(&linalg::Vector::new(0.0, 2.0, 0.0)));
-        for y in range(0, height) {
-            for x in range(0, width) {
-                let px = (x as f32 + 0.5, y as f32 + 0.5);
-                let mut ray = camera.generate_ray(px);
-                match instance.intersect(&mut ray) {
-                    Some(_) => rt.write(px.0, px.1, &film::Colorf::broadcast(1.0)),
-                    None => {},
-                }
+    let instance = geometry::Instance::new(&sphere,
+        linalg::Transform::translate(&linalg::Vector::new(0.0, 2.0, 0.0)));
+    for y in range(0, height) {
+        for x in range(0, width) {
+            let px = (x as f32 + 0.5, y as f32 + 0.5);
+            let mut ray = camera.generate_ray(px);
+            match instance.intersect(&mut ray) {
+                Some(_) => rt.write(px.0, px.1, &film::Colorf::broadcast(1.0)),
+                None => {},
             }
         }
     }
