829dfe28bcd913e578ed40c6b6c946706b32165e
Will Usher <willusher.life@gmail.com>
2015-01-03 15:41:44 -0800

    Threaded test of block queue and multiple sender single receiver channel
    Plan to use the channel method to handle writing to the render target

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate tray_rust;
 
 use std::vec::Vec;
+use std::sync::{Arc, TaskPool};
+use std::io::timer;
+use std::time::duration::Duration;
 use tray_rust::linalg;
 use tray_rust::film;
 use tray_rust::geometry;
@@ -8,7 +11,46 @@ use tray_rust::geometry::Geometry;
 use tray_rust::sampler;
 use tray_rust::sampler::{Sampler};
 
+/// Trial of how we might do the render target stuff.
+/// Threads are each sent a send end of the channel that is
+/// read from by the render target thread which then saves the
+/// values recieved to the framebuffer
+fn thread_work(tx: Sender<uint>, i: uint, queue: Arc<sampler::BlockQueue>) {
+    for _ in queue.iter() {
+        tx.send(i);
+        // Pretend to do some work
+        timer::sleep(Duration::milliseconds(50));
+    }
+}
+
+/// Hand the workers their own send endpoints to communicate results back
+/// to the main thread but drop the sender on the main thread so once
+/// all threads finish the channel closes
+fn spawn_workers(pool: &TaskPool, n: uint) -> Receiver<uint> {
+    let (tx, rx) = channel();
+    let block_queue = Arc::new(sampler::BlockQueue::new((10, 10), (2, 2)));
+    for i in range(0, n) {
+        let t = tx.clone();
+        let q = block_queue.clone();
+        // Can we just directly pass thread_work here? How would we send the args?
+        pool.execute(move || {
+            thread_work(t, i, q);
+        });
+    }
+    rx
+}
+
+fn thread_test() {
+    let n = 8;
+    let pool = TaskPool::new(n);
+    let rx = spawn_workers(&pool, n);
+    for m in rx.iter() {
+        println!("Message: {}", m);
+    }
+}
+
 fn main() {
+    thread_test();
     let width = 800u;
     let height = 600u;
 
@@ -20,10 +62,10 @@ fn main() {
     let instance = geometry::Instance::new(&sphere,
         linalg::Transform::translate(&linalg::Vector::new(0.0, 2.0, 0.0)));
 
-    let mut block_queue = sampler::BlockQueue::new((width as u32, height as u32), (8, 8));
+    let block_queue = sampler::BlockQueue::new((width as u32, height as u32), (8, 8));
     let mut sampler = sampler::Uniform::new(block_queue.block_dim());
     let mut sample_pos = Vec::with_capacity(sampler.max_spp());
-    for b in block_queue {
+    for b in block_queue.iter() {
         sampler.select_block(&b);
         while sampler.has_samples() {
             sampler.get_samples(&mut sample_pos);
