79b6041ce64def2a211810e9ab71d04656f8854f
Will Usher <willusher.life@gmail.com>
2015-01-04 16:39:45 -0800

    Add integrator trait and module where we'll place various integrators
    Currently just have our same hit marker integrator as before to just
    shade all objects as flat white but will add Whitted once materials are
    implemented.

---
+++ src/integrator/mod.rs
@@ -0,0 +1,23 @@
+//! The integrator module defines the Integrator trait implemented by
+//! the various surface integrators used to render the scene with different
+//! integration methods, eg. path tracing, photon mapping etc.
+
+use scene::Scene;
+use linalg::Ray;
+use geometry::Intersection;
+use film::Colorf;
+
+pub use self::hit_marker::HitMarker;
+
+pub mod hit_marker;
+
+/// Trait implemented by the various integration methods that can be used to render
+/// the scene.
+pub trait Integrator {
+    /// Compute the illumination at the intersection in the scene
+    /// TODO: Later we'll need to pass `&mut Sampler` through here as well
+    /// for integrators that need randomness along with a memory pool once
+    /// we implement that as well.
+    fn illumination(&self, scene: &Scene, ray: &Ray, hit: &Intersection) -> Colorf;
+}
+
