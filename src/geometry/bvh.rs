965505e9fb125f935114cb0d25d00f64cbeb01f3
Will Usher <willusher.life@gmail.com>
2015-05-06 16:28:01 -0600

    Trying to pass a closure to the BVH but dealing with lifetime crap

--- src/geometry/bvh.rs
+++ src/geometry/bvh.rs
@@ -1,16 +1,31 @@
 //! Provides a simple SAH split based BVH2 that stores types implementing the Boundable trait
 
-use geometry::Boundable;
+use std::sync::Arc;
+use std::ops::FnMut;
 
-pub struct BVH<'a, T: 'a> {
+use geometry::Boundable;
+use linalg::Ray;
+
+pub struct BVH<T> {
     // Maximum amount of geometry we're willing to put in a leaf
     max_geom: usize,
-    objects: Vec<&'a T>,
+    objects: Arc<Vec<T>>,
 }
 
-impl<'a, T: Boundable> BVH<'a, T> {
-    pub fn new(max_geom: usize, objects: Vec<&'a T>) -> BVH<'a, T> {
+impl<T: Boundable> BVH<T> {
+    pub fn new(max_geom: usize, objects: Arc<Vec<T>>) -> BVH<T> {
         BVH { max_geom: max_geom, objects: objects }
+    }
+    /// Traverse the BVH and call the function passed on the objects in the leaf nodes
+    /// of the BVH, returning the value returned by the function after traversal
+    /// completes.
+    /// TODO: I've tried to base this interface on that of `Iterator::map` however they don't seem
+    /// to take `f` as a mutable parameter?
+    pub fn intersect<F, R>(&self, ray: &mut Ray, mut f: F) -> Option<R>
+            where F: FnMut(&mut Ray, &T) -> Option<R> {
+        let mut result = None;
+        result = f(ray, &self.objects[0]).or(result);
+        result
     }
 }
 
--- src/scene.rs
+++ src/scene.rs
@@ -11,12 +11,14 @@ use integrator;
 use integrator::Integrator;
 use light;
 use light::Light;
+use geometry::BVH;
 
 /// The scene containing the objects and camera configuration we'd like to render,
 /// shared immutably among the ray tracing threads
 pub struct Scene {
     pub camera: Arc<Camera>,
     pub instances: Arc<Vec<Instance>>,
+    pub bvh: BVH<Instance>,
     pub integrator: Arc<Box<Integrator + Send + Sync>>,
     /// TODO: Only one light for now
     pub light: Arc<Box<Light + Send + Sync>>,
@@ -31,7 +33,7 @@ impl Scene {
         let white_wall = Arc::new(Box::new(Matte::new(&Colorf::new(1.0, 1.0, 1.0), 1.0)) as Box<Material + Send + Sync>);
         let red_wall = Arc::new(Box::new(Matte::new(&Colorf::new(1.0, 0.2, 0.2), 1.0)) as Box<Material + Send + Sync>);
         let blue_wall = Arc::new(Box::new(Matte::new(&Colorf::new(0.2, 0.2, 1.0), 1.0)) as Box<Material + Send + Sync>);
-        let instances = vec![
+        let instances = Arc::new(vec![
             // The back wall
             Instance::new(plane.clone(), white_wall.clone(), Transform::translate(&Vector::new(0.0, 20.0, 12.0))
                           * Transform::scale(&Vector::broadcast(32.0)) * Transform::rotate_x(90.0), "back_wall"),
@@ -57,12 +59,13 @@ impl Scene {
                 Arc::new(Box::new(Glass::new(&Colorf::broadcast(1.0), &Colorf::broadcast(1.0), 1.52))
                      as Box<Material + Send + Sync>), Transform::translate(&Vector::new(6.0, -2.0, 5.0))
                     * Transform::scale(&Vector::broadcast(5.0)), "glass_sphere")
-        ];
+        ]);
         let light_color = Colorf::broadcast(200.0) * Colorf::new(0.780131, 0.780409, 0.775833);
         Scene {
             camera: Arc::new(Camera::new(Transform::look_at(&Point::new(0.0, -60.0, 12.0),
                 &Point::new(0.0, 0.0, 12.0), &Vector::new(0.0, 0.0, 1.0)), 30.0, (w, h))),
-            instances: Arc::new(instances),
+            instances: instances.clone(),
+            bvh: BVH::new(4, instances.clone()),
             integrator: Arc::new(Box::new(integrator::Path::new(3, 8)) as Box<Integrator + Send + Sync>),
             light: Arc::new(Box::new(light::Point::new(&Point::new(0.0, 0.0, 22.0), &light_color))
                             as Box<Light + Send + Sync>),
@@ -74,7 +77,8 @@ impl Scene {
         // We can always return the next hit found since the ray's max_t value is updated
         // after an intersection is found. Thus if we find another hit we know that one
         // occured before any previous ones. TODO: Replace with a BVH
-        self.instances.iter().fold(None, |p, ref i| i.intersect(ray).or(p))
+        self.bvh.intersect(&mut ray, |ref mut r, ref i| i.intersect(r))
+        //self.instances.iter().fold(None, |p, ref i| i.intersect(ray).or(p))
     }
 }
 
