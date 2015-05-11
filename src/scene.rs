57fe4ff2bf098243e557ad3d6b3ebfd610b7cb89
Will Usher <willusher.life@gmail.com>
2015-05-11 16:07:09 -0600

    Comment out the rust logo, Stanford Buddha, Dragon scene

--- src/scene.rs
+++ src/scene.rs
@@ -28,18 +28,44 @@ impl Scene {
     pub fn new(w: usize, h: usize) -> Scene {
         let sphere = Arc::new(Box::new(Sphere::new(1.0)) as Box<BoundableGeom + Send + Sync>);
         let plane = Arc::new(Box::new(Plane) as Box<BoundableGeom + Send + Sync>);
-        let mut models = Mesh::load_obj(Path::new("./rust-logo.obj"));
-        let mesh = Arc::new(Box::new(models.remove("rust_logo").unwrap()) as Box<BoundableGeom + Send + Sync>);
 
-        let white_wall = Arc::new(Box::new(Matte::new(&Colorf::new(1.0, 1.0, 1.0), 1.0)) as Box<Material + Send + Sync>);
-        let red_wall = Arc::new(Box::new(Matte::new(&Colorf::new(1.0, 0.2, 0.2), 1.0)) as Box<Material + Send + Sync>);
-        let blue_wall = Arc::new(Box::new(Matte::new(&Colorf::new(0.2, 0.2, 1.0), 1.0)) as Box<Material + Send + Sync>);
-        let merl_mat = Arc::new(Box::new(Merl::load_file(Path::new("two-layer-silver.binary"))) as Box<Material + Send + Sync>);
+        let white_wall = Arc::new(Box::new(Matte::new(&Colorf::new(0.75, 0.75, 0.75), 1.0))
+                                  as Box<Material + Send + Sync>);
+        let red_wall = Arc::new(Box::new(Matte::new(&Colorf::new(0.75, 0.25, 0.25), 1.0))
+                                as Box<Material + Send + Sync>);
+        let blue_wall = Arc::new(Box::new(Matte::new(&Colorf::new(0.25, 0.25, 0.75), 1.0))
+                                 as Box<Material + Send + Sync>);
+
+        /*
+        let mut models = Mesh::load_obj(Path::new("./rust-logo.obj"));
+        let rust_logo = Arc::new(Box::new(models.remove("rust_logo").unwrap()) as Box<BoundableGeom + Send + Sync>);
+
+        let mut models = Mesh::load_obj(Path::new("./buddha.obj"));
+        let buddha = Arc::new(Box::new(models.remove("buddha").unwrap()) as Box<BoundableGeom + Send + Sync>);
+
+        let mut models = Mesh::load_obj(Path::new("./dragon.obj"));
+        let dragon = Arc::new(Box::new(models.remove("dragon").unwrap()) as Box<BoundableGeom + Send + Sync>);
+
+        let oxidized_steel = Arc::new(Box::new(Merl::load_file(Path::new("black-oxidized-steel.binary")))
+                                      as Box<Material + Send + Sync>);
+        let gold_paint = Arc::new(Box::new(Merl::load_file(Path::new("gold-metallic-paint.binary")))
+                                  as Box<Material + Send + Sync>);
+        let blue_acrylic = Arc::new(Box::new(Merl::load_file(Path::new("blue-acrylic.binary")))
+                                    as Box<Material + Send + Sync>);
+        */
 
         let instances = vec![
-            Instance::new(mesh.clone(), merl_mat.clone(), Transform::translate(&Vector::new(0.0, 5.0, 12.0))
+            /*
+            Instance::new(rust_logo, oxidized_steel, Transform::translate(&Vector::new(0.0, 6.0, 12.0))
                           * Transform::rotate_z(180.0) * Transform::rotate_y(90.0)
-                          * Transform::scale(&Vector::broadcast(12.0)), "model"),
+                          * Transform::scale(&Vector::broadcast(12.0)), "rust_logo"),
+            Instance::new(buddha, gold_paint, Transform::translate(&Vector::new(-9.0, 0.5, 7.55))
+                          * Transform::rotate_z(-140.0) * Transform::rotate_x(90.0)
+                          * Transform::scale(&Vector::broadcast(17.0)), "buddha"),
+            Instance::new(dragon, blue_acrylic, Transform::translate(&Vector::new(8.25, 1.0, 3.7))
+                          * Transform::rotate_z(120.0) * Transform::rotate_x(90.0)
+                          * Transform::scale(&Vector::broadcast(13.0)), "dragon"),
+            */
             // The back wall
             Instance::new(plane.clone(), white_wall.clone(), Transform::translate(&Vector::new(0.0, 20.0, 12.0))
                           * Transform::scale(&Vector::broadcast(32.0)) * Transform::rotate_x(90.0), "back_wall"),
@@ -55,7 +81,6 @@ impl Scene {
             // The bottom wall
             Instance::new(plane.clone(), white_wall.clone(), Transform::translate(&Vector::new(0.0, 0.0, 0.0))
                           * Transform::scale(&Vector::broadcast(32.0)), "bottom_wall"),
-            /*
             // The reflective sphere
             Instance::new(sphere.clone(),
             Arc::new(Box::new(SpecularMetal::new(&Colorf::new(0.155, 0.116, 0.138), &Colorf::new(4.828, 3.122, 2.146)))
@@ -66,7 +91,6 @@ impl Scene {
             Arc::new(Box::new(Glass::new(&Colorf::broadcast(1.0), &Colorf::broadcast(1.0), 1.52))
                      as Box<Material + Send + Sync>), Transform::translate(&Vector::new(6.0, -2.0, 5.0))
                      * Transform::scale(&Vector::broadcast(5.0)), "glass_sphere")
-            */
         ];
         let light_color = Colorf::broadcast(200.0) * Colorf::new(0.780131, 0.780409, 0.775833);
         Scene {
@@ -74,7 +98,7 @@ impl Scene {
                 &Point::new(0.0, 0.0, 12.0), &Vector::new(0.0, 0.0, 1.0)), 30.0, (w, h)),
             bvh: BVH::new(4, instances),
             integrator: Arc::new(Box::new(integrator::Path::new(4, 8)) as Box<Integrator + Send + Sync>),
-            light: Arc::new(Box::new(light::Point::new(&Point::new(0.0, -5.0, 14.0), &light_color))
+            light: Arc::new(Box::new(light::Point::new(&Point::new(0.0, 0.0, 22.0), &light_color))
                             as Box<Light + Send + Sync>),
         }
     }
