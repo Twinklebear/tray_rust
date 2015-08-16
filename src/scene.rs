2386b9afafa3e0c63193985834ead587877c4f0e
Will Usher <willusher.life@gmail.com>
2015-08-16 09:47:40 -0600

    Add support for grouping objects to apply a base transformation

--- src/scene.rs
+++ src/scene.rs
@@ -273,6 +273,14 @@ fn load_objects(path: &Path, materials: &HashMap<String, Arc<Material + Send + S
                                      .expect("Geometry is required for receivers"));
 
             instances.push(Instance::receiver(geom, mat, transform, name));
+        } else if ty == "group" {
+            let group_objects = o.find("objects").expect("A group must specify an array of objects in the group");
+            let mut group_instances = load_objects(path, materials, group_objects);
+            for mut gi in group_instances {
+                let t = *gi.get_transform();
+                gi.set_transform(transform * t);
+                instances.push(gi);
+            }
         } else {
             panic!("Error parsing object '{}': unrecognized type '{}'", name, ty);
         }
