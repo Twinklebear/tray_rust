e527d0b7f66c7dcb6ffaf142f370d8ffef44660c
Will Usher <willusher.life@gmail.com>
2015-09-12 16:22:11 -0600

    Fix normals on meshes with per-vertex normals.
    I don't do a separate shading geometry computation (maybe for bump
    mapping later?). For now just add in a DiffGeom that actually uses the
    normal we pass it for shading instead of using dp_du x dp_dv which gives
    the face normal.

--- src/geometry/differential_geometry.rs
+++ src/geometry/differential_geometry.rs
@@ -7,7 +7,7 @@ use geometry::Geometry;
 /// Stores information about a hit piece of geometry of some object in the scene
 #[derive(Clone, Copy)]
 pub struct DifferentialGeometry<'a> {
-    /// The hit point 
+    /// The hit point
     pub p: Point,
     /// The shading normal
     pub n: Normal,
@@ -22,13 +22,20 @@ pub struct DifferentialGeometry<'a> {
 }
 
 impl<'a> DifferentialGeometry<'a> {
-    /// Initialize the differential geometry with 0 values for all fields
-    /// and None for the hit geometry
+    /// Setup the differential geometry. Note that the normal will be computed
+    /// using cross(dp_du, dp_dv)
     pub fn new(p: &Point, ng: &Normal, dp_du: &Vector, dp_dv: &Vector,
                geom: &'a (Geometry + 'a))
                -> DifferentialGeometry<'a> {
         let n = linalg::cross(dp_du, dp_dv).normalized();
         DifferentialGeometry { p: *p, n: Normal::new(n.x, n.y, n.z), ng: *ng,
+                               dp_du: *dp_du, dp_dv: *dp_dv, geom: geom }
+    }
+    /// Setup the differential geometry using the normal passed for the surface normal
+    pub fn with_normal(p: &Point, n: &Normal, dp_du: &Vector, dp_dv: &Vector,
+               geom: &'a (Geometry + 'a))
+               -> DifferentialGeometry<'a> {
+        DifferentialGeometry { p: *p, n: *n, ng: *n,
                                dp_du: *dp_du, dp_dv: *dp_dv, geom: geom }
     }
 }
