ea580622d3c2f5a10496742fc116c0fccbab72cf
Will Usher <willusher.life@gmail.com>
2015-10-05 22:29:10 -0600

    Switch animated color to store things in a vec as well

--- src/film/animated_color.rs
+++ src/film/animated_color.rs
@@ -1,7 +1,6 @@
 //! Provides an animated color value, so you can have colors change over time
 
 use std::cmp::{Eq, Ord, PartialOrd, PartialEq, Ordering};
-use std::collections::BTreeSet;
 
 use linalg;
 use film::Colorf;
@@ -40,33 +39,30 @@ impl PartialEq for ColorKeyframe {
 #[derive(Debug, Clone)]
 pub struct AnimatedColor {
     /// List of color keyframes in time order
-    keyframes: BTreeSet<ColorKeyframe>,
+    keyframes: Vec<ColorKeyframe>,
 }
 
 impl AnimatedColor {
-    /// Create a new empty animated color
-    pub fn new() -> AnimatedColor {
-        AnimatedColor { keyframes: BTreeSet::new() }
-    }
     /// Create an animated transform that will blend between the passed keyframes
-    pub fn with_keyframes(keyframes: Vec<ColorKeyframe>) -> AnimatedColor {
-        AnimatedColor { keyframes: keyframes.into_iter().collect() }
+    pub fn with_keyframes(mut keyframes: Vec<ColorKeyframe>) -> AnimatedColor {
+        keyframes.sort();
+        AnimatedColor { keyframes: keyframes }
     }
     /// Compute the color at the desired time
     pub fn color(&self, time: f32) -> Colorf {
         if self.keyframes.is_empty() {
             Colorf::black()
         } else if self.keyframes.len() == 1 {
-            self.keyframes.iter().next().unwrap().color
+            self.keyframes[0].color
         } else {
             // TODO: Binary search here somehow? Or does the BTreeSet have some faster impl
             // of take/skip while?
             let first = self.keyframes.iter().take_while(|k| k.time < time).last();
             let second = self.keyframes.iter().skip_while(|k| k.time < time).next();
             if first.is_none() {
-                self.keyframes.iter().next().unwrap().color
+                self.keyframes.first().unwrap().color
             } else if second.is_none() {
-                self.keyframes.iter().last().unwrap().color
+                self.keyframes.last().unwrap().color
             } else {
                 let mut color = Colorf::black();
                 let f = first.unwrap().color;
