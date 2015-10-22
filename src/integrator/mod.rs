6b1f9a4c013c22962d99cb57969083f0dc8eaa8e
Will Usher <willusher.life@gmail.com>
2015-10-22 08:51:23 -0600

    Update to rust nightly 1.5 to get fat ptr comparisons
    I would update to beta but vec::resize is still not stable!?

--- src/integrator/mod.rs
+++ src/integrator/mod.rs
@@ -149,9 +149,7 @@ pub trait Integrator {
                 match scene.intersect(&mut ray) {
                     Some(h) => {
                         if let &Instance::Emitter(ref e) = h.instance {
-                            // TODO: The extra case to *const () is to workaround the ICE I
-                            // encountered writing this code: https://github.com/rust-lang/rust/issues/2744/
-                            if e as *const Light as *const () == light as *const Light as *const () {
+                            if e as *const Light == light as *const Light {
                                 li = e.radiance(&-w_i, &h.dg.p, &h.dg.n, time)
                             }
                         }
