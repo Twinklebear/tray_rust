c9bf3201f66e29f32febd4683f3e07bc96f83c23
Will Usher <willusher.life@gmail.com>
2016-03-23 14:21:57 -0600

    Fix typo breaking build

--- src/bxdf/specular_transmission.rs
+++ src/bxdf/specular_transmission.rs
@@ -61,7 +61,7 @@ impl BxDF for SpecularTransmission {
             };
         let w_i = Vector::new(eta * -w_o.x, eta * -w_o.y, cos_t);
         let f = self.fresnel.fresnel(bxdf::cos_theta(w_o));
-        let c = (Colorf::broadcast(1.0) - f * self.transmission / f32::abs(bxdf::cos_theta(&w_i));
+        let c = (Colorf::broadcast(1.0) - f) * self.transmission / f32::abs(bxdf::cos_theta(&w_i));
         (c, w_i, 1.0)
     }
 }
