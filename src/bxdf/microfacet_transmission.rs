660140af458eda50604f8c948c3a0968edb64d49
Will Usher <willusher.life@gmail.com>
2016-08-07 18:40:53 -0500

    Rough glass almost right, not quite Blender but could be some other dif?
    The rough glass might actually be right and there's some other
    difference with Blender that causes us to not quite match.

--- src/bxdf/microfacet_transmission.rs
+++ src/bxdf/microfacet_transmission.rs
@@ -31,10 +31,25 @@ impl MicrofacetTransmission {
     /// we're entering or exiting this material based on the direction of the outgoing
     /// ray.
     fn eta_for_interaction(&self, w_o: &Vector) -> (f32, f32) {
-        if w_o.z > 0.0 {
+        if bxdf::cos_theta(w_o) > 0.0 {
             (self.fresnel.eta_i, self.fresnel.eta_t)
         } else {
             (self.fresnel.eta_t, self.fresnel.eta_i)
+        }
+    }
+    /// Compute the Jacobian for the change of variables (see [Walter et al 07] section 4.2),
+    /// here we compute equation 17 in that section.
+    fn jacobian(w_o: &Vector, w_i: &Vector, w_h: &Vector, eta: (f32, f32)) -> f32 {
+        let wi_dot_h = linalg::dot(w_i, w_h);
+        let wo_dot_h = linalg::dot(w_o, w_h);
+        f32::powf(eta.1, 2.0) * f32::abs(wo_dot_h) / f32::powf(eta.0 * wi_dot_h + eta.1 * wo_dot_h, 2.0)
+    }
+    fn half_vector(w_o: &Vector, w_i: &Vector, eta: (f32, f32)) -> Option<Vector> {
+        let mut w_h = -eta.0 * *w_o - eta.1 * *w_i;
+        if w_h.x == 0.0 && w_h.y == 0.0 && w_h.z == 0.0 {
+            None
+        } else {
+            Some(w_h.normalized())
         }
     }
 }
@@ -46,66 +61,60 @@ impl BxDF for MicrofacetTransmission {
         e.insert(BxDFType::Transmission);
         e
     }
+    // etaA is on the outside, etaB is on the inside
+    // so etaA = eta.0, etaB = eta.1
+    // in walter eta al eta_i is incident side, eta_o/eta_t is transmitted side
+    // so eta_i = eta.0, eta_o & eta_t = eta.1
     fn eval(&self, w_o: &Vector, w_i: &Vector) -> Colorf {
-        let cos_to = f32::abs(bxdf::cos_theta(w_o));
-        let cos_ti = f32::abs(bxdf::cos_theta(w_i));
+        if bxdf::same_hemisphere(w_o, w_i) {
+            return Colorf::black();
+        }
+        let cos_to = bxdf::cos_theta(w_o);
+        let cos_ti = bxdf::cos_theta(w_i);
         if cos_to == 0.0 || cos_ti == 0.0 {
-            return Colorf::new(0.0, 0.0, 0.0)
+            return Colorf::black();
         }
-        let mut w_h = *w_i + *w_o;
-        if w_h.x == 0.0 && w_h.y == 0.0 && w_h.z == 0.0 {
-            return Colorf::new(0.0, 0.0, 0.0)
-        }
-        w_h = w_h.normalized();
         let eta = self.eta_for_interaction(w_o);
-        let d = self.microfacet.normal_distribution(&w_h);
-        let f = Colorf::broadcast(1.0) - self.fresnel.fresnel(linalg::dot(w_i, &w_h));
-        let g = self.microfacet.shadowing_masking(w_i, w_o, &w_h);
-        let wi_dot_h = linalg::dot(w_i, &w_h);
-        let wo_dot_h = linalg::dot(w_o, &w_h);
-        self.reflectance
-            * (f32::abs(wi_dot_h) * f32::abs(wo_dot_h)) / (f32::abs(w_i.z) * f32::abs(w_o.z))
-            * (f32::powf(eta.1, 2.0) * f * g * d) / f32::powf(eta.0 * wi_dot_h + eta.1 * wo_dot_h, 2.0)
-    }
-    fn sample(&self, w_o: &Vector, samples: &(f32, f32)) -> (Colorf, Vector, f32) {
-        let w_h = match self.microfacet.sample(w_o, samples) {
-            h if !bxdf::same_hemisphere(w_o, &h) => -h,
-            h => h
-        };
-        let eta = self.eta_for_interaction(w_o);
-        let w_i = match linalg::refract(w_o, &w_h, eta.0, eta.1) {
-            Some(v) => v,
-            None => return (Colorf::black(), Vector::broadcast(0.0), 0.0),
-        };
-        if bxdf::same_hemisphere(w_o, &w_i) {
-            (Colorf::black(), Vector::broadcast(0.0), 0.0)
+        if let Some(w_h) = MicrofacetTransmission::half_vector(w_o, w_i, eta) {
+            let d = self.microfacet.normal_distribution(&w_h);
+            let f = Colorf::broadcast(1.0) - self.fresnel.fresnel(linalg::dot(w_o, &w_h));
+            let g = self.microfacet.shadowing_masking(w_i, w_o, &w_h);
+            let wi_dot_h = linalg::dot(w_i, &w_h);
+            let jacobian = MicrofacetTransmission::jacobian(w_o, w_i, &w_h, eta);
+            self.reflectance * (f32::abs(wi_dot_h) / (f32::abs(w_i.z) * f32::abs(w_o.z)))
+                * (f * g * d) * jacobian
         } else {
-            // This term is p_o(o) in eq. 38 of Walter et al's 07 paper and is for reflection so
-            // we use the Jacobian for reflection, eq. 17
-            let wi_dot_h = linalg::dot(&w_i, &w_h);
-            let wo_dot_h = linalg::dot(w_o, &w_h);
-            let jacobian = eta.1 * f32::abs(linalg::dot(w_o, &w_h))
-                / f32::powf(eta.1 * wi_dot_h + eta.0 * wo_dot_h, 2.0);
-            let pdf = self.microfacet.pdf(&w_h) * jacobian;
-            (self.eval(w_o, &w_i), w_i, pdf)
+            Colorf::black()
+        }
+    }
+    // NOTE: w_o is in the material we're coming from, w_i is in the material we're entering
+    fn sample(&self, w_o: &Vector, samples: &(f32, f32)) -> (Colorf, Vector, f32) {
+        let mut w_h = self.microfacet.sample(w_o, samples);
+        if !bxdf::same_hemisphere(w_o, &w_h) {
+            w_h = -w_h;
+        }
+        let eta = self.eta_for_interaction(w_o);
+        if let Some(w_i) = linalg::refract(w_o, &w_h, eta.0, eta.1) {
+            if bxdf::same_hemisphere(w_o, &w_i) {
+                (Colorf::black(), Vector::broadcast(0.0), 0.0)
+            } else {
+                let pdf = self.microfacet.pdf(&w_h)
+                    * MicrofacetTransmission::jacobian(w_o, &w_i, &w_h, eta);
+                (self.eval(w_o, &w_i), w_i, pdf)
+            }
+        } else {
+            (Colorf::black(), Vector::broadcast(0.0), 0.0)
         }
     }
     fn pdf(&self, w_o: &Vector, w_i: &Vector) -> f32 {
         if bxdf::same_hemisphere(w_o, w_i) {
             0.0
         } else {
-            let w_h = *w_o + *w_i;
-            if w_h.x == 0.0 && w_h.y == 0.0 && w_h.z == 0.0 {
-                0.0
+            let eta = self.eta_for_interaction(w_o);
+            if let Some(w_h) = MicrofacetTransmission::half_vector(w_o, w_i, eta) {
+                self.microfacet.pdf(&w_h) * MicrofacetTransmission::jacobian(w_o, w_i, &w_h, eta)
             } else {
-                let eta = self.eta_for_interaction(w_o);
-                let wi_dot_h = linalg::dot(w_i, &w_h);
-                let wo_dot_h = linalg::dot(w_o, &w_h);
-                // This term is p_o(o) in eq. 38 of Walter et al's 07 paper and is for reflection so
-                // we use the Jacobian for reflection, eq. 17
-                let jacobian = eta.1 * f32::abs(linalg::dot(w_o, &w_h))
-                    / f32::powf(eta.1 * wi_dot_h + eta.0 * wo_dot_h, 2.0);
-                self.microfacet.pdf(&w_h.normalized()) * jacobian
+                0.0
             }
         }
     }
